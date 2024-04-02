e59a647b31b9b90df7898d917745689b6ee577d8
Marto <53458858+mhasmat@users.noreply.github.com>
2024-04-02 19:54:36 -0300

    upload views tarea

--- ejercicios_practica/marvel/e_commerce/views.py
+++ ejercicios_practica/marvel/e_commerce/views.py
@@ -10,6 +10,12 @@ def comic_list_api_view(request):
         # Seguir los pasos detallados en
         # el archivo de enunciado de tarea
         print("Endpoint: comic_list_api_view")
+        # traigo todos los comics a mi query
+        queryset = Comic.objects.all()
+        # convierto los valores de la query a dict y luego a list
+        _data = list(queryset.values())
+                # retorno un json
+        return JsonResponse(data=_data, safe=False, status=200)
     else:
         return JsonResponse(data={"message": "Método HTTP no permitido."}, status=405)
 
@@ -21,6 +27,12 @@ def comic_filter_stock_api_view(request):
         # Seguir los pasos detallados en
         # el archivo de enunciado de tarea
         print("Endpoint: comic_filter_stock_api_view")
+        # hago la query filtrada
+        queryset = Comic.objects.filter(stock_qty=5)
+        # serializo a lista de diccionarios
+        _data = list(queryset.values())
+        # retorno la data
+        return JsonResponse(data=_data, safe=False, status=200)
     else:
         return JsonResponse(data={"message": "Método HTTP no permitido."}, status=405)
 
@@ -32,6 +44,12 @@ def comic_filter_price_api_view(request):
         # Seguir los pasos detallados en
         # el archivo de enunciado de tarea
         print("Endpoint: comic_filter_price_api_view")
+        # mi query
+        queryset = Comic.objects.filter(price__gt=3)
+        # serializo
+        _data = list(queryset.values())
+
+        return JsonResponse(data=_data, safe=False, status=200)
     else:
         return JsonResponse(data={"message": "Método HTTP no permitido."}, status=405)
 
@@ -43,5 +61,11 @@ def comic_list_order_api_view(request):
         # Seguir los pasos detallados en
         # el archivo de enunciado de tarea
         print("Endpoint: comic_list_order_api_view")
+        # query
+        queryset = Comic.objects.all().order_by('marvel_id')
+        # serialize
+        _data = list(queryset.values())
+
+        return JsonResponse(data=_data, safe=False, status=200)
     else:
         return JsonResponse(data={"message": "Método HTTP no permitido."}, status=405)
